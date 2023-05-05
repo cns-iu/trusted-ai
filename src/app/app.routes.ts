00acb778dc8d92b4cc2984025ae84eec8a54a670
edlu77 <eddd.lu@gmail.com>
2023-05-05 04:27:38 -0400

    Set up landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () => LandingPageComponent,
+  },
+];
