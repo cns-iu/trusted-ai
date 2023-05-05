0c004a21a1e76acc2079431c82667283f659e6e9
edlu77 <eddd.lu@gmail.com>
2023-05-05 06:30:14 -0400

    Make footer component

---
+++ src/app/page-footer/page-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'trust-ai-page-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './page-footer.component.html',
+  styleUrls: ['./page-footer.component.scss'],
+})
+export class PageFooterComponent {}
