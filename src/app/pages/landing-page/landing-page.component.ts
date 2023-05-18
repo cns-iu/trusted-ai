afb5f6c82fce2558828a21603cdb6fa3767218a8
edlu77 <eddd.lu@gmail.com>
2023-05-18 05:27:42 -0400

    Add search box to landing page

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { SearchBoxComponent } from 'src/app/search-box/search-box.component';
 
 @Component({
   selector: 'trust-ai-landing-page',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, SearchBoxComponent],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss'],
 })
