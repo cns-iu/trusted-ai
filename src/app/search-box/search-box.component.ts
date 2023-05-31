1bd35878aa650b239fa3e53a236f4813b81c43d3
edlu77 <eddd.lu@gmail.com>
2023-05-31 05:48:52 -0400

    Styling changes

--- src/app/search-box/search-box.component.ts
+++ src/app/search-box/search-box.component.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, EventEmitter, HostBinding, Input, Output } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
 import { MatOptionModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { map, startWith } from 'rxjs';
-import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { map, startWith } from 'rxjs';
 
 @Component({
   selector: 'trust-ai-search-box',
