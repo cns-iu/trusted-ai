e089dd3ff42c615439c76911e1a1eae6ac7632c0
edlu77 <eddd.lu@gmail.com>
2023-06-05 13:11:07 -0400

    Fix faq link

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 /**
  * Application navbar
@@ -8,7 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'trust-ai-navbar',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
