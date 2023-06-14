a38e641d0517179d3bb36e5483ccd85a2d20d570
edlu77 <eddd.lu@gmail.com>
2023-06-13 21:58:20 -0400

    Add preparation level section to profile

--- src/queries/job_descriptions.sql
+++ src/queries/job_descriptions.sql
@@ -1,6 +1,32 @@
 CREATE TEMP VIEW foo AS
-SELECT soc_id, title, descr, alt_titles
-FROM public.profile_sec_one;
+SELECT
+  a.soc_id,
+  a.soc_nems_id,
+  a.title,
+  a.descr,
+  c.job_zone,
+  d.name,
+  d.experience,
+  d.education,
+  d.job_training,
+  d.example,
+  d.svp_range,
+  string_agg(alt_title, ', '::text) AS alt_titles
+  FROM public.occs a
+    LEFT JOIN alt_titles b ON a.soc_id = b.soc_id
+	  LEFT JOIN job_zones c ON a.soc_id = c.soc_id
+    LEFT JOIN job_zone_refs d ON c.job_zone = d.job_zone
+WHERE
+  a."grouping" = 'Detailed Occupation'::text OR a."grouping" = 'Detailed Occupation Specialization'::text
+GROUP BY
+	a.soc_id,
+	c.job_zone,
+	d.name,
+	d.experience,
+	d.education,
+	d.job_training,
+  d.example,
+	d.svp_range;
 
 \t
 \a
