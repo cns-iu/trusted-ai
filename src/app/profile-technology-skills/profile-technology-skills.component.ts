c37ed1a6898e4fcb650aff5d57c8a8876ceed22e
edlu77 <eddd.lu@gmail.com>
2023-06-23 05:53:11 -0400

    Testing and documentation

--- src/app/profile-technology-skills/profile-technology-skills.component.ts
+++ src/app/profile-technology-skills/profile-technology-skills.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, DoCheck, EventEmitter, Input, Output } from '@angular/core';
 
+/**
+ * Technology skills list of job profile
+ */
 @Component({
   selector: 'trust-ai-profile-technology-skills',
   standalone: true,
@@ -9,14 +12,19 @@ import { Component, DoCheck, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./profile-technology-skills.component.scss'],
 })
 export class ProfileTechnologySkillsComponent implements DoCheck {
+  /** All job skills */
   @Input() allSkills: [string, string[]][] = [];
 
+  /** If all skills should be shown */
   @Input() showAll = false;
 
+  /** Emits when show all is clicked */
   @Output() showAllButtonClick = new EventEmitter();
 
+  /** Job skills that are currently displayed */
   skillsShown: [string, string[]][] = [];
 
+  /** Updates displayed skills whenever the list of skills or the show all setting is changed */
   ngDoCheck() {
     this.skillsShown = this.showAll ? this.allSkills : this.allSkills.slice(0, 3);
   }
