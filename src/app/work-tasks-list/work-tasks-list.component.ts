28ed6c304c5954e02bad1ebb777c2ddf383044f6
edlu77 <eddd.lu@gmail.com>
2023-06-27 05:32:37 -0400

    Create work tasks list component

---
+++ src/app/work-tasks-list/work-tasks-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'trust-ai-work-tasks-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './work-tasks-list.component.html',
+  styleUrls: ['./work-tasks-list.component.scss'],
+})
+export class WorkTasksListComponent {}
