617b24b7c9b990ffafd7dfba55c633433facb634
edlu77 <edlu77@users.noreply.github.com>
2023-07-06 02:10:48 +0000

    Production deploy -- 9cf55e52d60c7173ceb34bd806cc1ef051878f01

--- compodoc/trust-ai/js/menu-wc.js
+++ compodoc/trust-ai/js/menu-wc.js
@@ -65,6 +65,9 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="components/CareerCardComponent.html" data-type="entity-link" >CareerCardComponent</a>
                             </li>
                             <li class="link">
+                                <a href="components/FaqPageComponent.html" data-type="entity-link" >FaqPageComponent</a>
+                            </li>
+                            <li class="link">
                                 <a href="components/LandingPageComponent.html" data-type="entity-link" >LandingPageComponent</a>
                             </li>
                             <li class="link">
@@ -86,7 +89,16 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="components/ProfilePageComponent.html" data-type="entity-link" >ProfilePageComponent</a>
                             </li>
                             <li class="link">
+                                <a href="components/ProfileSalaryComponent.html" data-type="entity-link" >ProfileSalaryComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ProfileTechnologySkillsComponent.html" data-type="entity-link" >ProfileTechnologySkillsComponent</a>
+                            </li>
+                            <li class="link">
                                 <a href="components/SearchBoxComponent.html" data-type="entity-link" >SearchBoxComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/WorkTasksListComponent.html" data-type="entity-link" >WorkTasksListComponent</a>
                             </li>
                         </ul>
                     </li>
@@ -105,7 +117,16 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="interfaces/JobInfo.html" data-type="entity-link" >JobInfo</a>
                             </li>
                             <li class="link">
+                                <a href="interfaces/SalaryInfo.html" data-type="entity-link" >SalaryInfo</a>
+                            </li>
+                            <li class="link">
                                 <a href="interfaces/SearchFilters.html" data-type="entity-link" >SearchFilters</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/TechSkill.html" data-type="entity-link" >TechSkill</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/WorkTasks.html" data-type="entity-link" >WorkTasks</a>
                             </li>
                         </ul>
                     </li>
@@ -117,6 +138,9 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             <span class="icon ion-ios-arrow-down"></span>
                         </div>
                         <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/functions.html" data-type="entity-link">Functions</a>
+                            </li>
                             <li class="link">
                                 <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
                             </li>
