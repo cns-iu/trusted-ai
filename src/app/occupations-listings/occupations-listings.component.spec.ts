2e37573f5f7cf94e611ebcb176f8217d6d68cdad
edlu77 <eddd.lu@gmail.com>
2023-07-31 15:09:22 -0400

    Add infinite scrolling to occupations page

--- src/app/occupations-listings/occupations-listings.component.spec.ts
+++ src/app/occupations-listings/occupations-listings.component.spec.ts
@@ -19,4 +19,28 @@ describe('OccupationsListingsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should start list with 50 jobs', () => {
+    component.allJobs = Array(100).fill({
+      'Alt Title': '',
+      Code: '',
+      'Data-level': '',
+      'Job Zone': '',
+      Occupation: '',
+    });
+    component.ngOnChanges();
+    expect(component.jobsToDisplay.length).toEqual(50);
+  });
+
+  it('add more jobs when scrolling', () => {
+    component.allJobs = Array(100).fill({
+      'Alt Title': '',
+      Code: '',
+      'Data-level': '',
+      'Job Zone': '',
+      Occupation: '',
+    });
+    component.onScrollDown();
+    expect(component.jobsToDisplay.length).toEqual(100);
+  });
 });
--- src/app/occupations-listings/occupations-listings.component.ts
+++ src/app/occupations-listings/occupations-listings.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 import { CareerCardComponent } from '../career-card/career-card.component';
 import { JobInfo } from '../pages/occupations-page/occupations-page.component';
@@ -8,14 +9,39 @@ import { JobInfo } from '../pages/occupations-page/occupations-page.component';
 @Component({
   selector: 'trust-ai-occupations-listings',
   standalone: true,
-  imports: [CommonModule, CareerCardComponent],
+  imports: [CommonModule, CareerCardComponent, InfiniteScrollModule],
   templateUrl: './occupations-listings.component.html',
   styleUrls: ['./occupations-listings.component.scss'],
 })
-export class OccupationsListingsComponent {
+export class OccupationsListingsComponent implements OnChanges {
   /** Jobs to be displayed in the list */
   @Input() allJobs: JobInfo[] | null | undefined = [];
 
   /** Emits job info when occupation is clicked */
   @Output() jobClicked = new EventEmitter<JobInfo>();
+
+  /** Jobs to display */
+  jobsToDisplay: JobInfo[] = [];
+
+  /** Current length of the jobs list */
+  currentLength = 50;
+
+  /**
+   * Updates job list on changes
+   */
+  ngOnChanges() {
+    if (this.allJobs) {
+      this.jobsToDisplay = this.allJobs.slice(0, this.currentLength);
+    }
+  }
+
+  /**
+   * Adds more jobs to display if user scrolls down enough
+   */
+  onScrollDown() {
+    this.currentLength += 50;
+    if (this.allJobs) {
+      this.jobsToDisplay = this.allJobs.slice(0, this.currentLength);
+    }
+  }
 }
